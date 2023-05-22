62f0bfb1af4ce1223761ff057870c170c2f49dd6
Collin Sherriff <collin@fanfire.ai>
2023-05-22 18:04:16 +0400

    start server code added

--- __name__.py
+++ __name__.py
@@ -12,17 +12,25 @@ app = Flask(__name__)
 @app.route('/analyze', methods=['POST'])  
 def analyze():
     zip_file = request.files['code_zip']   
+    
+    # Extract the zip file 
     with zipfile.ZipFile(zip_file, 'r') as zip_ref:
         zip_ref.extractall('tmp/')    
         
+    # Get the full code string 
     code = ''
     for filename in os.listdir('tmp'):
         with open(os.path.join('tmp', filename), 'r') as f:
             code += f.read()  
             
+    # Make request to OpenAI API    
     response = requests.post('https://api.openai.com/v1/engines/chatgpt/completions',   
         headers = {'Authorization': f'Bearer {API_KEY}'},
         data = {'prompt': code}
     )
     
-    return response.json()['choices'][0]['text'] 
+    # Return response
+    return response.json()['choices'][0]['text']
+
+if __name__ == '__main__': 
+    app.run(debug=True) 
